74dcca2213c9e4b0d9ad205f81d772c01a8c4a35
hdyess <henrypdyess@gmail.com>
2025-11-13 12:45:12 -0500

    windowing system almost done

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class UserInterface {
@@ -7,11 +8,15 @@ public class UserInterface {
 	private static ArrayList<Window> winRow1;
 	private static ArrayList<Window> winRow2;
 
-
+	//for now everything assumes windows within a row are of equal height
 	public static void displayWindows() {
+		ArrayList<StringBuilder> fullDisplayLines = new ArrayList<>();
 		for (Window w : winRow1) {
 
 		}
+
+		fullDisplayLines.forEach(stringBuilder -> System.out.println(stringBuilder.toString()));
+
 	}
 
 
--- src/main/java/com/pluralsight/Window.java
+++ src/main/java/com/pluralsight/Window.java
@@ -6,43 +6,75 @@ public class Window {
 
 	private int width;
 	private int height;
-	private int orderX;
-	private int orderY;
 	private ArrayList<String> rows;
+	StringBuilder rowBuilder;
 
-	public Window(int width, int height, int orderX, int orderY) {
+	public Window(int width, int height) {
 		this.width = width;
 		this.height = height;
-		this.orderX = orderX;
-		this.orderY = orderY;
-		rows = new ArrayList<String>(height);
+		rows = new ArrayList<String>();
+		this.rowBuilder = new StringBuilder();
+	}
+
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 
 	public ArrayList<String> getRows() {
+		if(rows.size()<this.height-1) {
+			for (int i = 0; i < this.height-1; i++) {
+				printToNextRow("");
+			}
+		}
 
+		//adds ending row
+		rowBuilder = new StringBuilder();
+		rowBuilder.append("/");
+		rowBuilder.append("-".repeat(this.width-2));
+//		for (int i = 0; i < this.width-2; i++) {
+//			rowBuilder.append("-");
+//		}
+		rowBuilder.append("\\");
+		rows.add(rowBuilder.toString());
+		//returns all rows
 		return rows;
 
 	}
 
-	public void printToNextRow(String lineToPrint) {
-		if( (rows.getFirst().equalsIgnoreCase("")) || rows.getFirst()==null) {
-			StringBuilder rowBuilder = new StringBuilder();
+	public void printToNextRow(String stringToPrint) {
+		if(rows.isEmpty()) {
+			rowBuilder = new StringBuilder();
 			rowBuilder.append("/");
-			for (int i = 0; i < this.width-2; i++) {
-				rowBuilder.append("-");
-			}
+			rowBuilder.append("-".repeat(this.width-2));
+//			for (int i = 0; i < this.width-2; i++) {
+//				rowBuilder.append("-");
+//			}
 			rowBuilder.append("\\");
 			rows.add(rowBuilder.toString());
 		}
 
-		StringBuilder rowBuilder = new StringBuilder();
+
+		rowBuilder = new StringBuilder();
 		rowBuilder.append("|");
-		rowBuilder.append(lineToPrint);
-		for (int i = 0; i < this.width-2; i++) {
-			rowBuilder.append();
+		rowBuilder.append(stringToPrint);
+		if (rowBuilder.length()<this.width-1) {
+			rowBuilder.append(" ".repeat(this.width-1-rowBuilder.length()));
+
+//			for (int i = 0; i <this.width-1-rowBuilder.length(); i++) {
+//				rowBuilder.append(" ");
+//			}
 		}
 		rowBuilder.append("|");
 		rows.add(rowBuilder.toString());
+	}
+
+	public void clearRows() {
+		this.rows = new ArrayList<>();
 	}
 
 
